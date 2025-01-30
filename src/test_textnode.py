44b96f2a567b1f071246ccd74e8c9da9683b8ed2
smugii23 <smugii0203@gmail.com>
2025-01-30 14:48:22 -0800

    Added test cases for text nodes

---
+++ src/test_textnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("Test node", TextType.ITALIC)
+        node2 = TextNode("Test node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+    def test_eq3(self):
+        node = TextNode("Test node!", TextType.TEXT)
+        node2 = TextNode("Test node?", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq4(self):
+        node = TextNode("Test node", TextType.TEXT, url="test.test")
+        node2 = TextNode("Test node", TextType.TEXT, url=None)
+        self.assertNotEqual(node, node2)
+        
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,23 +1,25 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal"
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
     LINKS = "links"
     IMAGES = "images"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
     
     def __eq__(self, other):
-        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
-            return True
-        return False
-    
+        return (
+            self.text == other.text and 
+            self.text_type == other.text_type and 
+            self.url == other.url
+        )
+        
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
