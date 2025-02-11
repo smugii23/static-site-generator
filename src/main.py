39f90fb5d531d5e09ed8f44290a6d1069a7afc69
smugii23 <smugii0203@gmail.com>
2025-02-11 08:55:28 -0800

    finished copy directory function

--- src/main.py
+++ src/main.py
@@ -3,10 +3,21 @@ import os, shutil
 def copy_static(source_dir, destination_dir):
     if os.path.exists(destination_dir):
         shutil.rmtree(destination_dir)
-        os.mkdir(destination_dir)
+    os.mkdir(destination_dir)
+    copy_recursive(source_dir, destination_dir)
+
+def copy_recursive(source, destination):
+    files = os.listdir(source)
+    for file in files:
+        if os.path.isfile(os.path.join(source, file)):
+            shutil.copy(os.path.join(source, file), destination)
+        else:
+            os.mkdir(os.path.join(destination, file))
+            copy_recursive(os.path.join(source, file), os.path.join(destination, file))
+
             
 
 def main():
-    print(TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev "))
+    copy_static('static', 'public')
 
 main()
