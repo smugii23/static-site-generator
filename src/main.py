1a806c558824c911f0632f87204751a0744c73a9
smugii23 <smugii0203@gmail.com>
2025-02-10 20:50:58 -0800

    started adding a copy from static to public function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+def copy_static(source_dir, destination_dir):
+    if os.path.exists(destination_dir):
+        shutil.rmtree(destination_dir)
+        os.mkdir(destination_dir)
+            
 
 def main():
     print(TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev "))
